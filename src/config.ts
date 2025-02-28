e11aa2b296553b45a528cefd54ed28d1d07c3fdd
J.Moffitt <90877462+Envcc@users.noreply.github.com>
2025-02-27 23:38:36 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,35 @@
-export const WebsiteName: string = "SaaS Starter"
-export const WebsiteBaseUrl: string = "https://saasstarter.work"
+// Config.ts
+
+export const WebsiteName: string = "SaaS Starter";
+export const WebsiteBaseUrl: string = "https://saasstarter.work";
 export const WebsiteDescription: string =
-  "Open source, fast, and free to host SaaS template. Built with SvelteKit, Supabase, Stripe, Tailwind, DaisyUI, and Postgres"
-export const CreateProfileStep: boolean = true
+  "Open source, fast, and free to host SaaS template. Built with SvelteKit, Supabase, Stripe, Tailwind, DaisyUI, and Postgres";
+
+export const primaryLink: string = "https://saasstarter.work/";
+export const primaryLinkText: string = "★ us on Github";
+
+export const blogLink: string = "/blog";
+export const blogText: string = "Blog";
+
+export const pricingLink: string = "/pricing";
+export const pricingText: string = "Pricing";
+
+export const accountLink: string = "/account";
+export const accountText: string = "Account";
+
+export const searchLink: string = "/search";
+export const searchLabel: string = "Search";
+export const searchLinkText: string = "Search";
+
+export const footerTitle: string = "Explore";
+export const aboutText: string = "Overview";
+export const net30PricingText: string = "Pricing";
+export const contactUsLink: string = "/contact_us";
+export const contactUsText: string = "Contact Us";
+
+export const sponsorLink: string = "https://github.com/CriticalMoments/CMSaasStarter";
+export const sponsorLinkText: string = "Github";
+
+export const sponsorName: string = "Kiln AI";
+export const sponsorDesc: string = "Build High Quality AI Products";
+export const sponsorLinkText: string = "Learn More";
